0b86cfcb8ebdc98adad7b48723907bd5cff1c1c0
Moco2669 <moxtheboom004@gmail.com>
2025-04-06 18:38:00 +0200

    order creation test refactor
    introduced constants for price name quantity etc

--- csharpcore/TellDontAskKata/TellDontAskKata.Tests/UseCase/OrderCreationUseCaseTest.cs
+++ csharpcore/TellDontAskKata/TellDontAskKata.Tests/UseCase/OrderCreationUseCaseTest.cs
@@ -14,25 +14,32 @@ namespace TellDontAskKata.Tests.UseCase
         private readonly IProductCatalog _productCatalog;
         private readonly OrderCreationUseCase _useCase;
 
+        private const string CategoryName = "food";
+        private const decimal CategoryTaxPercentage = 10m;
+        private const string Product1Name = "salad";
+        private const decimal Product1Price = 3.56m;
+        private const string Product2Name = "tomato";
+        private const decimal Product2Price = 4.65m;
+
         public OrderCreationUseCaseTest()
         {
             var food = new Category { 
-                Name = "food",
-                TaxPercentage = 10m
+                Name = CategoryName,
+                TaxPercentage = CategoryTaxPercentage
             };
 
             _productCatalog = new InMemoryProductCatalog(new List<Product>
             {
                 new Product
                 {
-                    Name = "salad",
-                    Price = 3.56m,
+                    Name = Product1Name,
+                    Price = Product1Price,
                     Category = food
                 },
                 new Product
                 {
-                    Name = "tomato",
-                    Price = 4.65m,
+                    Name = Product2Name,
+                    Price = Product2Price,
                     Category = food
                 }
             });
@@ -46,16 +53,18 @@ namespace TellDontAskKata.Tests.UseCase
         [Fact]
         public void SellMultipleItems()
         {
+            const int saladQuantity = 2;
             var saladRequest = new SellItemRequest
             {
-                ProductName = "salad",
-                Quantity = 2
+                ProductName = Product1Name,
+                Quantity = saladQuantity
             };
 
+            const int tomatoQuantity = 3;
             var tomatoRequest = new SellItemRequest
             {
-                ProductName = "tomato",
-                Quantity = 3
+                ProductName = Product2Name,
+                Quantity = tomatoQuantity
             };
 
             var request = new SellItemsRequest
@@ -71,14 +80,14 @@ namespace TellDontAskKata.Tests.UseCase
             Assert.Equal(2.13m, insertedOrder.Tax);
             Assert.Equal("EUR", insertedOrder.Currency);
             Assert.Equal(2, insertedOrder.Items.Count);
-            Assert.Equal("salad", insertedOrder.Items[0].Product.Name);
-            Assert.Equal(3.56m, insertedOrder.Items[0].Product.Price);
-            Assert.Equal(2, insertedOrder.Items[0].Quantity);
+            Assert.Equal(Product1Name, insertedOrder.Items[0].Product.Name);
+            Assert.Equal(Product1Price, insertedOrder.Items[0].Product.Price);
+            Assert.Equal(saladQuantity, insertedOrder.Items[0].Quantity);
             Assert.Equal(7.84m, insertedOrder.Items[0].TaxedAmount);
             Assert.Equal(0.72m, insertedOrder.Items[0].Tax);
-            Assert.Equal("tomato", insertedOrder.Items[1].Product.Name);
-            Assert.Equal(4.65m, insertedOrder.Items[1].Product.Price);
-            Assert.Equal(3, insertedOrder.Items[1].Quantity);
+            Assert.Equal(Product2Name, insertedOrder.Items[1].Product.Name);
+            Assert.Equal(Product2Price, insertedOrder.Items[1].Product.Price);
+            Assert.Equal(tomatoQuantity, insertedOrder.Items[1].Quantity);
             Assert.Equal(15.36m, insertedOrder.Items[1].TaxedAmount);
             Assert.Equal(1.41m, insertedOrder.Items[1].Tax);
         }
